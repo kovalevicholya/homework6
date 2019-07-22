e8b9989b94f4172dadf10f7abcfef785b671085a
kovalevicholya <52216058+kovalevicholya@users.noreply.github.com>
2019-07-22 22:25:54 +0300

    Delete Area.java

--- task_a_4/Area.java
+++
@@ -1,7 +0,0 @@
-package com.company.task_124_A_4;
-
-public class Area extends Region {
-    public Area(String name, double square, double population) {
-        super(name, square, population);
-    }
-}
